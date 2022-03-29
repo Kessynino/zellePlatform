2aed28ad5e3c40add40026d82f05590ff8f88a25
Kessynino <kessynino@gmail.com>
2022-03-29 11:16:55 -0500

    all

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBNKAGHMX2Z3cVuc1_V2w6gnklYfS7Uyak",
+    authDomain: "haitiedtech-1d85b.firebaseapp.com",
+    projectId: "haitiedtech-1d85b",
+    storageBucket: "haitiedtech-1d85b.appspot.com",
+    messagingSenderId: "745089334429",
+    appId: "1:745089334429:web:0b579a8e7263cb728c6874",
+    measurementId: "G-DDZ3ZHV4E7"
+  }
 };
 
 /*
@@ -13,4 +22,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+//import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
